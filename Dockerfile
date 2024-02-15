ff0567e3ffeb68124a44f91e8d7949dc125f69e1
MisterChief53 <soderol3@hotmail.com>
2024-02-14 18:10:46 -0600

    setup docker container using docker compose so that an example api works, together with the database

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM bellsoft/liberica-openjdk-debian:latest
+
+ENV PORT 8080
+# # set hostname to localhost
+ENV HOSTNAME "0.0.0.0"
---
+++ src/test/java/com/metapulse/accountsserver/AccountsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metapulse.accountsserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountsServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
