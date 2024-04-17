0afbc6b6ba917aa92e01e41688bbc5f4d77e07a4
Edson Ibarra <edsonibarra54@gmail.com>
2024-04-17 15:52:49 -0600

    Fixed bug where user items were not displayed on web page

--- src/main/java/com/metapulse/accountsserver/ItemController.java
+++ src/main/java/com/metapulse/accountsserver/ItemController.java
@@ -3,6 +3,7 @@ package com.metapulse.accountsserver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping(path="/items")
 public class ItemController {
     @Autowired
